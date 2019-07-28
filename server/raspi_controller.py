147551d96b966a5df2e0ce09f2a5b2b6648cb401
Tatsuki Moriizumi <tmoriizu@yahoo-corp.jp>
2019-07-28 16:59:29 +0900

    fix server

--- server/raspi_controller.py
+++ server/raspi_controller.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 
 baterrys = [18, 12]
-reds = [5,17, 23, 16]
-greens = [6,27, 24, 20]
-blues = [13,22, 25, 21]
+reds = [5, 17, 23, 16]
+greens = [6, 27, 24, 20]
+blues = [13, 22, 25, 21]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -18,42 +18,35 @@ def switchLed(arg1, arg2, arg3):
 	GPIO.output(arg2[0], arg2[1])
 	GPIO.output(arg3[0], arg3[1])
 
-def batteryOn():
+def setup():
 	for battery in baterrys:
 		GPIO.setup(battery, GPIO.OUT)
 		GPIO.output(battery, True)
 
-def run():
-	batteryOn()
-	for i in range(1):
-		print 'ON'
-		for(red, green, blue) in zip (reds, greens, blues):
-			GPIO.setup(red,GPIO.OUT)
-			GPIO.setup(green,GPIO.OUT)
-			GPIO.setup(blue,GPIO.OUT)
-			ledOn = convertDict([red, False], [green, True], [blue, True])
-			switchLed(**ledOn)
-		time.sleep(1)
+	for (red, green, blue) in zip(reds, greens, blues):
+		GPIO.setup(red, GPIO.OUT)
+		GPIO.setup(green, GPIO.OUT)
+		GPIO.setup(blue, GPIO.OUT)
 
-		for(red, green, blue) in zip (reds, greens, blues):
-			ledOn = convertDict([red, True], [green, False], [blue, True])
-			switchLed(**ledOn)
-		time.sleep(1)
+def connectRed():
+	for (red, green, blue) in zip(reds, greens, blues):
+		ledOn = convertDict([red, False], [green, True], [blue, True])
+		switchLed(**ledOn)
 
-		for(red, green, blue) in zip (reds, greens, blues):
-			ledOn = convertDict([red, True], [green, True], [blue, False])
-			switchLed(**ledOn)
-		time.sleep(1)
+def connectAll():
+	for (red, green, blue) in zip(reds, greens, blues):
+		ledOn = convertDict([red, False], [green, False], [blue, False])
+		switchLed(**ledOn)
 
-		for (red, green, blue) in zip(reds, greens, blues):
-			ledOn = convertDict([red, False], [green, False], [blue, False])
-			switchLed(**ledOn)
-		time.sleep(1)
-
-		for(red, green, blue) in zip (reds, greens, blues):
-			ledOff = convertDict([red, True], [green, True], [blue, True])
-			switchLed(**ledOff)
-
+def disconnect():
+	for (red, green, blue) in zip(reds, greens, blues):
+		ledOff = convertDict([red, True], [green, True], [blue, True])
+		switchLed(**ledOff)
 
 if __name__ == '__main__':
-    run()
+	setup()
+	connectAll()
+	time.sleep(1)
+	connectRed()
+	time.sleep(1)
+	disconnect()
--- server/raspi_server.py
+++ server/raspi_server.py
@@ -6,22 +6,40 @@ from raspi_controller import *
 def recieveSocket(host='172.20.10.5', port=5000):
     s = socket.socket()
     s.bind((host, port))
+    setup()
 
-    flg = True
-    while flg:
+    try:
         print 'listening'
         s.listen(5)
         c, addr = s.accept()
-        print 'receiving'
-        recieve = c.recv(4096)
-        print recieve
-        print 'sending'
-        c.send(recieve)
-        if(recieve == b'1'):
-        	run()
-            flg = False
-            c.close()
-    s.close()
+
+        flg = True
+        while flg:
+            print 'receiving'
+            recieve = c.recv(4096).decode()
+            print recieve
+
+            print 'sending'
+            c.send("from server: " + recieve)
+
+            if(recieve == '0'):
+                connectRed()
+            elif(recieve == '1'):
+                connectAll()
+            elif(recieve == '2'):
+                disconnect()
+            elif(recieve == 'fin'):
+                flg = False
+                c.close()
+            else:
+                pass
+        s.close()
+
+    except Exception as e:
+        print e
+        c.close()
+        s.close()
+
 
 if __name__ == '__main__':
     recieveSocket()
