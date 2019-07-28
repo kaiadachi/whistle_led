db40ab6c045e8f9ca690cbca3f23a4ee7d1b59c4
kai adachi <kaadachi@yahoo-corp.jp>
2019-07-28 19:38:58 +0900

    done model

--- client/common/model_3class.py
+++ client/common/model_3class.py
@@ -30,18 +30,18 @@ class Model:
 
         # optimizer
         self.train_step = tf.compat.v1.train.AdamOptimizer(1e-4).minimize(self.loss)
-        
+
         self.saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables())
 
         self.sess = tf.compat.v1.Session()
-        
+
         self.sess.run(tf.compat.v1.global_variables_initializer())
 
     def init_model(self):
-        num_fc1 = 128
-        num_fc2 = 128
-        num_fc3 = 128
-        
+        num_fc1 = 64
+        num_fc2 = 64
+        num_fc3 = 64
+
         w_fc1 = weight_variable( [self.input_size, num_fc1] )
         b_fc1 = bias_variable([num_fc1])
         h_fc1 = tf.nn.relu( tf.matmul(self.x, w_fc1) + b_fc1 )
@@ -56,7 +56,7 @@ class Model:
 
         w_fc4 = weight_variable( [num_fc3, self.output_size] )
         b_fc4 = bias_variable( [self.output_size] )
-        
+
         self.y_softmax = tf.nn.softmax(tf.matmul(h_fc3, w_fc4) + b_fc4)
 
     def get_softmax(self, inp):
@@ -64,10 +64,10 @@ class Model:
         return output
 
     def get_pred(self, inp, label):
-        pred = self.sess.run( tf.cast(self.correct_prediction, tf.float32), 
+        pred = self.sess.run( tf.cast(self.correct_prediction, tf.float32),
             feed_dict={self.x:inp, self.labels:label})
         return pred
-    
+
     def get_maxindex(self,inp):
         return tf.argmax( self.get_softmax(inp),1)
 
@@ -79,5 +79,3 @@ class Model:
 
     def load_model(self, fname):
         self.saver.restore(self.sess, fname)
-
-
